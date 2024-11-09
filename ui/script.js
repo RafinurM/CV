d9038febce165a2ed0dc05795249952414775964
Rafinur Mursalimov <RMursalimov2@slb.com>
2024-11-09 17:39:53 +0700

    add projects

--- ui/script.js
+++ ui/script.js
@@ -1,1 +1,46 @@
-const about = document.querySelector('.about');
+const projectList = Array.from(document.querySelectorAll('.project_element-link'));
+const buttonNext = document.querySelector('.next');
+const buttonPrev = document.querySelector('.prev');
+let id = 0;
+
+init();
+
+function init() {
+    addActiveClass(id);
+}
+
+function addActiveClass(id) {
+    projectList[id].classList.add('project_element-link-active');
+}
+
+function switchLinkNext() {
+    clearActiveClass(projectList);
+    if (id >= projectList.length - 1) {
+        id = -1;
+    }
+    id = id + 1;
+    addActiveClass(id);
+}
+
+function switchLinkPrev() {
+    clearActiveClass(projectList);
+    if (id <= 0) {
+        id = projectList.length;
+    }
+    id = id - 1;
+    addActiveClass(id);
+}
+
+function clearActiveClass(projectList) {
+    projectList.forEach(project => {
+        project.classList.remove('project_element-link-active');
+    })
+}
+buttonNext.addEventListener('click', () => {
+    switchLinkNext(projectList);
+});
+
+buttonPrev.addEventListener('click', () => {
+    switchLinkPrev(projectList);
+});
+
